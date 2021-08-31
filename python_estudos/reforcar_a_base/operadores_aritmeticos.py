c86e9e594c887b13b9743734cfc199445f7900b9
Alisson Gustavo <gustavoalisson112@gmail.com>
2021-08-31 16:22:04 -0300

    operadores aritmeticos

---
+++ python_estudos/reforcar_a_base/operadores_aritmeticos.py
@@ -0,0 +1,30 @@
+
+# * OPERADORES DO PYTHON 
+
+"""
+* (+) Adição
+* (-) Subtração
+* (*) Multilicaçãp 
+* (/) Divisão 
+* (**) Potência
+* (//) Divisão Inteira
+* (%) Resto da divisão
+
+"""
+print(5 + 2)
+print(5 - 2)
+print(5 * 2)
+print(5 / 2)
+print(5 ** 2)
+print(5 // 2)
+print(5 % 2)
+
+# * ORDEM PRECEDÊNCIA
+
+"""
+* 1. ()
+* 2. **
+* 3. * / // %
+* 4. + -
+
+"""
