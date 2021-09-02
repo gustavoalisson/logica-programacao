94feb90c83b0f79575f111b9f1bc01861af17577
Alisson Gustavo <gustavoalisson112@gmail.com>
2021-09-02 11:25:53 -0300

    tabuada

---
+++ python_estudos/tabuada_1a10.py
@@ -0,0 +1,16 @@
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+number = int(input('Digite um número: '))
+
+for i in lista:
+    soma = number + i
+    mult = number * i
+    div = number // i
+    subtr = number - i
+    
+    print(f'{number} + {i} = {soma}')
+    # print(f'{number} X {i} = {mult}')
+    # print(f'{number} / {i} = {div}')
+    # print(f'{number} - {i} = {subtr}')
+    
+    
+
