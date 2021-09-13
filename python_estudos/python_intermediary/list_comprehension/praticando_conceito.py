e61c3e3d96ca97908594c9211ee550b86e2df464
gustavoalisson <52181576+gustavoalisson@users.noreply.github.com>
2021-09-13 16:14:58 -0300

    Continuação dos estudos de List Comprehension

--- python_estudos/python_intermediary/list_comprehension/praticando_conceito.py
+++ python_estudos/python_intermediary/list_comprehension/praticando_conceito.py
@@ -2,14 +2,20 @@ list_empyt = []
 new_list = [ list_empyt.append(item) for item in range(1, 5) ]
 print(list_empyt)
 
+# --------------------------------------------------------------
+
 lista = []
 lista2 = []
 
 for i in [1,2,3]:
     for j in [4,5,6]:
-        lista.append((i,j))
+        if i % 2 == 0:
+            lista.append((i,j))
 
 print(lista)   
 
-comprehension = [ lista2.append((i,j)) for i in [1,2,3] for j in [4,5,6] ] 
-print(lista2)        
+comprehension = [ lista2.append((i,j)) for i in [1,2,3] for j in [4,5,6] if i % 2 == 0] 
+print(lista2)
+
+lista3 = [ [ (i,j) for i in [1,2,3] for j in [4,5,6] if i % 2 == 0 ] for v in range(0,3) ] 
+print(lista3)       
