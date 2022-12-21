80a72e453b84f48ff7692e98a565fcad84e23b6a
gustavoalisson <gustavoalisson112@gmail.com>
2022-12-21 17:30:12 -0300

    introdução rapida sobre string

---
+++ course_python_3_udemy/aula3.py
@@ -0,0 +1,25 @@
+"""
+
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+String são textos que estão dentro de aspas
+
+"""
+
+print(1234)
+
+# Aspas Simples
+print('Alisson G')
+
+# Aspas duplas
+print("Alisson Gustavo")
+
+# Escape
+print("Alisson \"Brincando Escape\" ")
+
+# r
+print(r"Alisson \"Brincando Escape\" ")
+
+# Mais fácil
+print('Alisson "Gustavo"')
+print(r'Alisson \"Gustavo"\ ')
