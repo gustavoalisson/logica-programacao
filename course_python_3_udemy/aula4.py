4a9fae945da14c5a403c3ee2a4749d33f4d225bf
gustavoalisson <gustavoalisson112@gmail.com>
2022-12-21 17:41:49 -0300

    introducao rapida para tipo int e float

---
+++ course_python_3_udemy/aula4.py
@@ -0,0 +1,17 @@
+# Tipos int e float
+# int -> número inteiro
+# O tipo int representa qualquer número
+# positivo ou negativo. int sem sinal é considerado positivo
+print(11) 
+print(-11)
+print(2000)
+
+# float
+print(51.10)
+
+# type
+
+print(type(5))
+print(type(5.0))
+print(type(.0))
+
