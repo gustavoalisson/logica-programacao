799b220ce474f2fc576c4b513013706d640c0ffc
gustavoalisson <gustavoalisson112@gmail.com>
2023-03-02 00:37:53 -0300

    organizando repo / novas aulas / atualizando estrutura de pastas

---
+++ course_python_3_udemy/iniciando_programacao_python_logica_basica/aula5.py
@@ -0,0 +1,14 @@
+# Tipo de dado bool (boolean)
+# Ao questionar algo em um programa
+# Só existem duas respostas possíveis:
+# sim (True) ou não (False).
+# Existem vários operadores para "questionar".
+# Dentro eles o ==, que é um operador lógico que
+# questiona se um valor é igual ao outro
+print(10 == 10) # True
+print(10 == 11) # False
+# type bool
+print(type(True))
+print(type(False))
+print(type(10 == 10)) 
+print(type(10 == 11))
