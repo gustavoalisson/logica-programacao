4d798cd0146d32a19536329930885a090a40e8d8
gustavoalisson <gustavoalisson112@gmail.com>
2023-03-02 21:31:42 -0300

    exercicio simples de imc e apresentando f-strings e algumas possibilidades

---
+++ course_python_3_udemy/iniciando_programacao_python_logica_basica/aula12.py
@@ -0,0 +1,6 @@
+nome = 'Alisson Gustavo'
+altura = 1.77
+peso = 80
+imc = peso / (altura ** 2)
+
+print(f'IMC de {nome} = {imc:.2f}')
---
+++ course_python_3_udemy/iniciando_programacao_python_logica_basica/aula13.py
@@ -0,0 +1,8 @@
+# Essa aula foi mostrado f-strings
+
+nome = 'Alisson'
+sobrenome = 'Gustavo'
+idade = 25
+
+usando_f_string = f'nome: {nome} {sobrenome}, idade: {idade}'
+print(usando_f_string)
