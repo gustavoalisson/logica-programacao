b966bdb0def23de23bf228e53629b86b8ce81033
gustavoalisson <gustavoalisson112@gmail.com>
2023-03-22 22:46:39 -0300

    subindo aula de debuger e aula de operadores relacionais

---
+++ course_python_3_udemy/iniciando_programacao_python_logica_basica/aula18.py
@@ -0,0 +1,24 @@
+# Na aula 18 foi mostrado como usar o Debug no VS Code
+
+# if / elif    /else
+
+condicao1 = False
+condicao2 = True
+condicao3 = False
+condicao4 = False
+
+if condicao1:
+    print('Código para a condição 1')
+elif condicao2:
+    print('Código para a condição 2')
+elif condicao3:
+    print('Código para a condição 3')
+elif condicao4:
+    print('Código para a condição 4')             
+else:
+    print('Nenhuma condição foi satisfeita.')
+    
+print('fora do if')    
+
+
+# Exemplo executado...
