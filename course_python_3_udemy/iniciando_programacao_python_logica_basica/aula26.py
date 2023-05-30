8c5c561c3752a5f652e7dbb269af77abef5ec795
gustavoalisson <gustavoalisson112@gmail.com>
2023-05-30 19:31:44 -0300

    explorando f string e outras possibilidades de manipular valores

---
+++ course_python_3_udemy/iniciando_programacao_python_logica_basica/aula26.py
@@ -0,0 +1,25 @@
+"""
+Interpolação básica de strings
+
+s - string
+d - int
+f - float
+.<número de dígitos>f
+x e X - Hexadecimal
+(Caractere)(><^)(quantidade)
+> - Esquerda 
+< - Direita
+^ - Centro
+Sinal de + ou - 
+Ex.: 0>-100,.1f
+Conversion flags - !r !s !a
+
+"""
+
+variavel = "ABC"
+print(f'.{variavel:>20}.')
+print(f'.{variavel:<20}.')
+print(f'.{variavel:q^20}.')
+print(f'{10000.564941261321:+,.1f}')
+
+
