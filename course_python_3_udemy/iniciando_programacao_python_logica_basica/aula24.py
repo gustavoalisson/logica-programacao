b9818f7963d3592cb052d93a6058b7ec97bc2b10
gustavoalisson <gustavoalisson112@gmail.com>
2023-05-30 18:36:38 -0300

    python basico, in e not in

---
+++ course_python_3_udemy/iniciando_programacao_python_logica_basica/aula24.py
@@ -0,0 +1,21 @@
+# Operador in e not in 
+# Strings em Python são iteráveis 
+
+# 0 1 2 3 4 5
+# A l i s s o n
+
+
+nome = 'Alisson'
+
+print(nome[1])
+print(nome[-6])
+
+print(15 * '-')
+
+print('ss' in nome)
+print('A' and "s" in nome)
+
+print(15 * '-')
+
+print('ss' not in nome)
+print('A' and "s" not in nome)
