f412617799f772b7413e9871772287f3e13ad2ef
gustavoalisson <gustavoalisson112@gmail.com>
2023-07-04 21:51:02 -0300

    exercicio do curso para testar conhecimento

---
+++ course_python_3_udemy/iniciando_programacao_python_logica_basica/aula28.py
@@ -0,0 +1,39 @@
+"""
+Exercício
+Peça ao usuário para digitar seu nome
+Peça ao usuário para digitar sua idade
+Se nome e idade forem digitados:
+    Exiba:
+        Seu nome é {nome}
+        Seu nome invertido é {nome invertido}
+        Seu nome contém (ou não) espaços
+        Seu nome tem {n} letras
+        A primeira letra do seu nome é {letra}
+        A última letra do seu nome é {letra}
+Se nada for digitado em nome ou idade: 
+    exiba "Desculpe, você deixou campos vazios."
+"""
+
+nome = input('Por favor, digite o seu nome: ')
+idade = input('Por favor, digite a sua idade: ')
+
+if nome and idade:
+
+    print(f'O seu nome é {nome}')
+    print(f'Seu nome invertido é {nome[::-1]}')
+    if " " in nome:  
+        print(f'Seu nome contém espaços ')
+    else: 
+        print(f'Seu nome não contém espaços.')
+    if " " in nome:                
+        print(f'O seu nome tem {len(nome.strip())} letras')
+    print(f'A primeira letra do seu nome é {nome[0]}')
+    print(f'A ultima letra do seu nome é {nome[-1]}')
+else:
+    print("Desculpe, você deixou campos vazios.")
+
+
+    
+    
+    
+    
